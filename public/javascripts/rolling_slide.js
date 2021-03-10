85237891914284cd55fea8b71ebd5a3652bd1473
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-03-10 19:28:15 +0900

    keyword search 완성 & crong 리뷰 적용

--- public/javascripts/rolling_slide.js
+++ public/javascripts/rolling_slide.js
@@ -5,11 +5,9 @@ const ON = "on";
 const rolling_list_box = _.$(".rolling_list_box");
 
 setInterval(() => {
-    rolling_list_box.classList.add(TRANS_TOP);
-    rolling_list_box.classList.add(ON);
+    rolling_list_box.classList.add(TRANS_TOP,ON);
     setTimeout (() => {
         rolling_list_box.insertBefore(rolling_list_box.firstElementChild, null);
-        rolling_list_box.classList.remove(TRANS_TOP);
-        rolling_list_box.classList.remove(ON);
+        rolling_list_box.classList.remove(TRANS_TOP,ON);
     },300)
 },3000);
--- public/javascripts/top10_function.js
+++ public/javascripts/top10_function.js
@@ -5,55 +5,75 @@ const input_box = _.$(".search_bar");
 const search_input = _.$(".search_input");
 const search_bar_list = _.$(".search_bar_list");
 const lists = _.$all(".lists");
+const keyword_search_list_box = _.$(".keyword_search_list_box");
+const keyword_search_list = _.$(".keyword_search_list");
 
 const bar_color = "search_bar_color";
-const search_bar_list_off = "search_bar_list_off";
+const display_off = "display_off";
+
 let count;
 
 document.addEventListener("click", (e) => {
-    let close = e.target.closest(".search_bar") ? true : false;
+    let close = !!e.target.closest(".search_bar");
     let contain = input_box.classList.contains("search_bar_color");
-
+    
     if(close && !contain) {
         input_box.classList.add(bar_color);
-        search_bar_list.classList.remove(search_bar_list_off);
+        search_bar_list.classList.remove(display_off);
+        if(search_input.value !== "") {
+            keyword_search_list_box.classList.remove(display_off);
+        }
     }
 })
 
+// search_bar_outline.addEventListener("mouseout", () => {
+//     input_box.classList.remove(bar_color);
+//     search_bar_list.classList.add(display_off);
+// })
+
+
 document.addEventListener("mouseover", (e) => {
-    let close = e.target.closest(".search_bar") ? true : false;
+    let close = !!e.target.closest(".search_bar");
     let box_in = e.target.closest(".search_bar_list");
 
     if(!close && !box_in) {
          input_box.classList.remove(bar_color);
-         search_bar_list.classList.add(search_bar_list_off);
+         search_bar_list.classList.add(display_off);
+         keyword_search_list_box.classList.add(display_off);
+         if(search_input.value === "") {
+            keyword_search_list_box.classList.add(display_off);
+         } else if(close && box_in && search_input.value !== "") {
+            keyword_search_list_box.classList.remove(display_off);
+         }
     }
  })
 
 export const top_slide = (titles) => {
+    // for => forEach 로 수정 완료
     const top_list = titles.top_10.list;
-
-    for(let i=0; i<top_list.length; i++) {
-        lists[i].innerHTML = `<a href="#">${top_list[i].rank} ${top_list[i].title}</a>`;
-    }
+    top_list.forEach((value,index) => {
+        lists[index].innerHTML = `<a href="#">${value.rank} ${value.title}</a>`;
+    })
 }
 
 export const rolling_keyword = (titles) => {
+    // for => reduce로 수정완료
     const top_list = titles.top_10.list;
-    let str = '';
+    const str = top_list.reduce((acc,cur) => {
+        acc = acc + `<div class="rolling_list">${cur.rank} ${cur.title}</div>`;
+        return acc;
+    },"");
 
-    for(let i=0; i<top_list.length; i++) {
-         str += `<div class="rolling_list">${top_list[i].rank} ${top_list[i].title}</div>`;
-    }
     rolling_list_box.insertAdjacentHTML("beforeend",str)
 }
 
 search_input.addEventListener("input", (e) => {
-    console.log(e.target.value)
     if(e.data !== null) {
-        rolling_list_box.classList.add(search_bar_list_off);
+        rolling_list_box.classList.add(display_off);
+        keyword_search_list_box.classList.remove(display_off);
     } else if(search_input.value == "") {
-        rolling_list_box.classList.remove(search_bar_list_off);
+        rolling_list_box.classList.remove(display_off);
+        keyword_search_list_box.classList.add(display_off);
     }
 
     if(count) clearTimeout(count);
@@ -61,10 +81,16 @@ search_input.addEventListener("input", (e) => {
         fetch(`https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${e.target.value}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`)
         .then(response => response.json())
         .then(json => keyword_search(json));
-    },500);
+    },0);
 })
 
+// 검색어 배열로 반환 후 화면에 표시
 const keyword_search = (word) => {
-    let lists = word.suggestions.map(v => v.value); // 배열에는 검색어에 해당하는 list만 담겨있다.
-    console.log(lists)
+    const lists = word.suggestions.map(v => v.value); // 배열에는 검색어에 해당하는 list만 담겨있다.
+    const str = lists.reduce((acc,cur) => {
+        acc = acc + `<li class="keyword_search_lists">${cur}</li>`;
+        return acc;
+    },"");
+    
+    keyword_search_list.innerHTML = str;
 }
